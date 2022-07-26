35ed51d7ec3edab0606b4a7b06bf16ffe1570f7c
Octavio Licea Leon <olicea@github.com>
2022-07-26 12:52:10 +0000

    test

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+import { Octokit } from '@octokit';
 
 try {
     const title  = core.getInput('title');
     const content = core.getInput('content');
     console.log(`Discussion ${title}: ${content}!`);
 
-    //create a new issue
+    // creating a discussion in GitHub
     const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
-    const issue = {
-        title: title,
-        body: content
-    };
-    octokit.issues.create({
-        owner: github.context.repo.owner,
+    // const discussion = {
+    //     title: title,
+    //     body: content
+    // };
+
+    // octokit.discussions.createDiscussion({
+    //     repo: github.context.repo.repo,
+    //     body: discussion
+    // });
+
+    octokit.rest.createDiscussion({
         repo: github.context.repo.repo,
-        issue_data: issue
+        body: discussion
+    }).then(res => {
+        console.log(res.data);
     });
+
 
 } catch (error) {
   core.setFailed(error.message);
