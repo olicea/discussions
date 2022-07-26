ca80c102d832e1876de8688efd8f811aa8c21642
Octavio Licea Leon <olicea@github.com>
2022-07-26 12:39:27 +0000

    creating an issue to test

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ try {
     const title  = core.getInput('title');
     const content = core.getInput('content');
     console.log(`Discussion ${title}: ${content}!`);
+
+    //create a new issue
+    const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
+    const issue = {
+        title: title,
+        body: content
+    };
+    octokit.issues.create({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        issue_data: issue
+    });
+
 } catch (error) {
   core.setFailed(error.message);
 }
