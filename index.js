f6278966532c6d317f0e673ac8381ef088b9fac0
Octavio Licea Leon <olicea@github.com>
2022-07-26 03:07:37 +0000

    logging

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  console.log(`Test!`);
+    const title  = core.getInput('title');
+    const content = core.getInput('content');
+    console.log(`Discussion ${title}: ${content}!`);
 } catch (error) {
   core.setFailed(error.message);
 }
